57294e59b4d96160d367c02dec164db62793d4a9
Aulia Rahman Hanifan <aulia.hanifan@traveloka.com>
2021-12-25 16:21:21 +0700

    add auth impl controller

---
+++ src/controllers/impl/auth.impl.controller.ts
@@ -0,0 +1,27 @@
+import { UserInput } from '../../dtos/inputs/user.input';
+import { AuthService } from '../../services/api/auth.api.service';
+import { BaseResponse, createResponse, HttpStatus, ServiceStatus }  from '../../utils';
+import { AuthController } from '../api/auth.api.controller';
+
+export class AuthControllerImpl implements AuthController {
+    private _authService : AuthService;
+
+    constructor(authService: AuthService) {
+        this._authService = authService;
+    }
+    async login(req: any): Promise<BaseResponse> {
+        const jsonBody = req.body;
+        const result = await this._authService.login(UserInput.fromObject(jsonBody));
+        
+        if (result.status == ServiceStatus.OK) {
+            return createResponse(HttpStatus.OK, result.body);
+        }
+
+        return createResponse(HttpStatus.BadRequest, null);
+    }
+
+    // NOT YET IMPLEMENTED
+    async refreshToken(req: any): Promise<BaseResponse> {
+        throw new Error('Method not implemented.');
+    }
+}
